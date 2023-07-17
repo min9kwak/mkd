f25129752c435808209ebff57a519cd09e823776
min9kwak <min9kwak@korea.ac.kr>
2023-07-17 12:14:12 -0400

    workstation2 gpu setting

--- utils/gpu.py
+++ utils/gpu.py
@@ -1,11 +1,13 @@
 import os
 
-STR2GPU = {'00': 'MIG-f3304720-4601-5894-bee4-cd0174024e06',
-           '10': 'MIG-35d812cd-2b57-5e55-bb30-890bd9675846',
-           '20': 'MIG-dc45e153-fb1e-5b2d-8a31-d3fb9494cd80',
-           '21': 'MIG-21d343f4-de6e-5d44-9774-e2f3dbab968d',
-           '30': 'MIG-0b2452d4-9b27-530f-a6f1-1c2d05dfaa72',
-           '31': 'MIG-e46a8085-268f-5417-8e5a-a9e20578424d'}
+STR2GPU = {"00": "MIG-f3304720-4601-5894-bee4-cd0174024e06",
+           "10": "MIG-35d812cd-2b57-5e55-bb30-890bd9675846",
+           "20": "MIG-f166d6fd-d51b-5e97-bbd0-99028db1cd2d",
+           "21": "MIG-d592d9d5-7cd0-5220-8695-37ba249d0084",
+           "30": "MIG-dc45e153-fb1e-5b2d-8a31-d3fb9494cd80",
+           "31": "MIG-21d343f4-de6e-5d44-9774-e2f3dbab968d",
+           "40": "MIG-0b2452d4-9b27-530f-a6f1-1c2d05dfaa72",
+           "41": "MIG-e46a8085-268f-5417-8e5a-a9e20578424d"}
 
 
 def set_gpu(config):
